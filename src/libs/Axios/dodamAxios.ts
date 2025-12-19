ea0939cb4d5f10e7df68b016672b4147a0ffa8e0
dlskl <skyandme77@gmail.com>
2025-12-19 21:35:55 +0900

    refactor : refactor dodamAxios #6

--- src/libs/Axios/dodamAxios.ts
+++ src/libs/Axios/dodamAxios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
-import config from "src/config/config.json"
-import { ACCESS_TOKEN_KEY,REQUEST_TOKEN_KEY } from "src/constants/Token/token.constants";
+import config from "src/config/config.json";
+import {
+  ACCESS_TOKEN_KEY,
+  REQUEST_TOKEN_KEY,
+} from "src/constants/Token/token.constants";
 import cookies from "../Cookie/cookie";
 import { dodamAxiosErrorInterceptor } from "./interceptors";
 
@@ -8,12 +11,19 @@ export const dodamAxios = axios.create({
   baseURL: config.SERVER,
   headers: {
     "Access-Control-Allow-Origin": "*",
-    [REQUEST_TOKEN_KEY]: `Bearer ${cookies.getCookie(ACCESS_TOKEN_KEY)!}`,
   },
+});
+
+dodamAxios.interceptors.request.use((config) => {
+  const token = cookies.getCookie(ACCESS_TOKEN_KEY);
+  if (token) {
+    config.headers[REQUEST_TOKEN_KEY] = `Bearer ${token}`;
+  }
+  return config;
 });
 
 dodamAxios.interceptors.response.use((res) => res, dodamAxiosErrorInterceptor);
 
 export const injectCustomAxiosAccessToken = (token: string) => {
-  dodamAxios.defaults.headers.common[REQUEST_TOKEN_KEY] = token;
+  dodamAxios.defaults.headers.common[REQUEST_TOKEN_KEY] = `Bearer ${token}`;
 };
